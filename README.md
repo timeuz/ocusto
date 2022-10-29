d76d635e91ea530910f1053cee2bc90917d65df8
Timeu <timeu.fo@gmail.com>
2022-10-29 18:21:52 -0300

    conversão para Python/Flask

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # ocusto
-Aplicação em PHP que calcula o melhor custo/benefício entre Gasolina e Etanol baseado no fato de que se o preço do litro do etanol dividido pelo da gasolina seja menor ou igual a 0,70 (ou seja, 70%), o etanol é mais vantajoso; se for superior, seria a gasolina mais vantajosa.
+Aplicação em Python e Flask que calcula o melhor custo/benefício entre Gasolina e Etanol baseado no fato de que se o preço do litro do etanol dividido pelo da gasolina seja menor ou igual a 0,73 (ou seja, 73%), o etanol é mais vantajoso; se for superior, seria a gasolina mais vantajosa.
 
 [Dependendo da tecnologia do veículo, no entanto, esse percentual pode variar para 75%.](https://autopapo.uol.com.br/noticia/porcentagem-gasolina-ou-etanol/)
---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def calccomb():
+    etanol = 0
+    gasolina = 0
+    result = 0
+    comp = 0.73
+    if request.method == 'POST':
+        etanol = float(request.form['etanol'])
+        gasolina = float(request.form['gasolina'])
+        # etanol = float(etanol)
+        # gasolina = float(gasolina)
+        
+        result = (etanol / gasolina)
+    
+    return render_template("index.html",
+                          etanol = etanol,
+                          gasolina = gasolina,
+                          result = result,
+                          comp = comp
+                          )
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)

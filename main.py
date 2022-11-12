c45a102771a56dc6873484df8e47360fd44397d0
timeuz <timeu.fo@gmail.com>
2022-11-11 21:33:35 -0300

    cerva done

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def calccomb():
         gasolina = float(request.form['gasolina'])
         result = (etanol / gasolina)
         result = float(f'{result:.2f}')
-    
+
     return render_template("comb.html",
                           etanol = etanol,
                           gasolina = gasolina,
@@ -23,7 +23,7 @@ def calccomb():
 
 @app.route('/cerva', methods=['GET', 'POST'])
 def calccerva():
-
+    resultado = ""
     result = {
         "litrao": 0,
         "seisct": 0,
@@ -32,71 +32,86 @@ def calccerva():
         "trezci": 0,
         "treznt": 0
     }
-
-    result["litrao"] = float(input("LITRAO: "))
-    result["seisct"] = float(input("600: "))
-    result["quinci"] = float(input("550: "))
-    result["trezcc"] = float(input("355: "))
-    result["trezci"] = float(input("350: "))
-    result["treznt"] = float(input("300: "))
-
-    for x in result.keys():
-        print(x)
-        match x:
-            case "litrao":
-                result[x] = result[x]/1000
-                print("to no litrao")
-            case "seisct":
-                result[x] = result[x]/600
-                print("to no seisct")
-            case "quinci":
-                result[x] = result[x]/550
-                print("to no quinci")
-            case "trezcc":
-                result[x] = result[x]/355
-                print("to no trezcc")
-            case "trezci":
-                result[x] = result[x]/350
-                print("to no trezci")
-            case "treznt":
-                result[x] = result[x]/300
-                print("to no treznt")
-
-    sortedresults = sorted(result.items(), key=lambda x:x[1])
-    sortedresults = dict(sortedresults)
-
-    for z in list(sortedresults):
-        if sortedresults[z] == 0.0:
-            del sortedresults[z]
-
-    print(sortedresults)
-
-        # result = float(f'{result:.2f}')
-    
-    return render_template("cerv.html",
-                          resultado = sortedresults[0],
-                          gasolina = gasolina,
-                          result = result,
-                          comp = comp
-                          )
-
-@app.route('/refri', methods=['GET', 'POST'])
-def calcrefri():
-    etanol = 0
-    gasolina = 0
-    result = 0
-    comp = 0.73
     if request.method == 'POST':
-        etanol = float(request.form['etanol'])
-        gasolina = float(request.form['gasolina'])
-        result = (etanol / gasolina)
-        result = float(f'{result:.2f}')
-    
-    return render_template("comb.html",
-                          etanol = etanol,
-                          gasolina = gasolina,
-                          result = result,
-                          comp = comp
-                          )
+        result = {
+            "litrao": float(request.form['litrao']),
+            "seisct": float(request.form['seisct']),
+            "quinci": float(request.form['quinci']),
+            "trezcc": float(request.form['trezcc']),
+            "trezci": float(request.form['trezci']),
+            "treznt": float(request.form['treznt'])
+        }
+
+        for x in result.keys():
+            match x:
+                case "litrao":
+                    result[x] = result[x]/1000
+                case "seisct":
+                    result[x] = result[x]/600
+                case "quinci":
+                    result[x] = result[x]/550
+                case "trezcc":
+                    result[x] = result[x]/355
+                case "trezci":
+                    result[x] = result[x]/350
+                case "treznt":
+                    result[x] = result[x]/300
+
+        sortedresults = sorted(result.items(), key=lambda x:x[1])
+        sortedresults = dict(sortedresults)
+
+        for z in list(sortedresults):
+            if sortedresults[z] == 0.0:
+                del sortedresults[z]
+
+        listkeys = list(sortedresults.keys())
+
+        match listkeys[0]:
+            case "litrao":
+                tipo = "Litrão"
+            case "seisct":
+                tipo = "Garrafa de 600ml"
+            case "quinci":
+                tipo = "Garrafa de 550ml"
+            case "trezcc":
+                tipo = "Lata de 355ml"
+            case "trezci":
+                tipo = "Lata de 350ml"
+            case "treznt":
+                tipo = "Lata de 300ml"
+
+        resultado = (sortedresults[listkeys[0]])
+        resultado = (f'Sai mais barato comprar {tipo} com o valor de R${resultado:{4}.{3}f} por ML')
+
+    return render_template(
+            "cerv.html",
+            resultado = resultado,
+            litrao = result["litrao"],
+            seisct = result["seisct"],
+            quinci = result["quinci"],
+            trezcc = result["trezcc"],
+            trezci = result["trezci"],
+            treznt = result["treznt"]
+        )
+
+# @app.route('/refri', methods=['GET', 'POST'])
+# def calcrefri():
+#     etanol = 0
+#     gasolina = 0
+#     result = 0
+#     comp = 0.73
+#     if request.method == 'POST':
+#         etanol = float(request.form['etanol'])
+#         gasolina = float(request.form['gasolina'])
+#         result = (etanol / gasolina)
+#         result = float(f'{result:.2f}')
+
+#     return render_template("comb.html",
+#                           etanol = etanol,
+#                           gasolina = gasolina,
+#                           result = result,
+#                           comp = comp
+#                           )
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
